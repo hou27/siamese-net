25407943dec42ef68df78bbec3021997fdf8ca77
hou27 <ataj125@gmail.com>
2023-05-20 22:16:47 +0900

    Update dataset path in test.py

---
+++ siamese-net/config.py
@@ -0,0 +1,10 @@
+# training_dir = "/content/drive/My Drive/data/sign_data/train"
+# training_csv = "/content/drive/My Drive/data/sign_data/train_data.csv"
+# testing_csv = "/content/drive/My Drive/data/sign_data/test_data.csv"
+# testing_dir = "/content/drive/My Drive/data/sign_data/test"
+training_dir = "/content/sign_data/train"
+training_csv = "/content/sign_data/train_data.csv"
+testing_csv = "/content/sign_data/test_data.csv"
+testing_dir = "/content/sign_data/test"
+batch_size = 32
+epochs = 20
