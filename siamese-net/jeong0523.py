49c19f40d1678a3e607203ab6630befa212fcc96
hou27 <ataj125@gmail.com>
2023-05-24 01:13:41 +0900

    학습된 모델을 통해 테스트

---
+++ siamese-net/jeong0523.py
@@ -0,0 +1,165 @@
+# import the necessary libraries
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as utils
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, Dataset
+import matplotlib.pyplot as plt
+import torchvision.utils
+import torchvision
+from torch.autograd import Variable
+from PIL import Image
+
+jeong_dir = "/content/drive/MyDrive/siamese-net/content/sign_data/jeong"
+batch_size = 32
+epochs = 20
+
+
+def imshow(img, text=None, should_save=False):
+    npimg = img.numpy()
+    plt.axis("off")
+    if text:
+        plt.text(
+            75,
+            8,
+            text,
+            style="italic",
+            fontweight="bold",
+            bbox={"facecolor": "white", "alpha": 0.8, "pad": 10},
+        )
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+# preprocessing and loading the dataset
+class SiameseDataset:
+    def __init__(self, training_dir=None, transform=None):
+        # used to prepare the labels and images path
+        # self.train_df = pd.read_csv(training_csv)
+        # self.train_df.columns = ["image1", "image2", "label"]
+        self.train_dir = training_dir
+        self.transform = transform
+
+    def __getitem__(self, index):
+        # getting the image path
+        # image1_path = os.path.join(self.train_dir, self.train_df.iat[index, 0])
+        image1_path = "/content/drive/MyDrive/siamese-net/content/sign_data/jeong/j1_converted.png"
+        # image2_path = os.path.join(self.train_dir, self.train_df.iat[index, 1])
+        # image2_path = "/content/drive/MyDrive/siamese-net/content/sign_data/jeong/j2_converted.png"
+        image2_path = "/content/drive/MyDrive/siamese-net/content/sign_data/jeong/j3_converted.png"
+
+        # Loading the image
+        img0 = Image.open(image1_path)
+        img1 = Image.open(image2_path)
+        img0 = img0.convert("L")
+        img1 = img1.convert("L")
+
+        # Apply image transformations
+        if self.transform is not None:
+            img0 = self.transform(img0)
+            img1 = self.transform(img1)
+
+        return (
+            img0,
+            img1,
+            torch.from_numpy(
+                # np.array([int(self.train_df.iat[index, 2])], dtype=np.float32)
+                np.array([1], dtype=np.float32)
+            ),
+        )
+
+    def __len__(self):
+        # return len(self.train_df)
+        return 1
+
+
+# create a siamese network
+class SiameseNetwork(nn.Module):
+    def __init__(self):
+        super(SiameseNetwork, self).__init__()
+        # Setting up the Sequential of CNN Layers
+        self.cnn1 = nn.Sequential(
+            nn.Conv2d(1, 96, kernel_size=11, stride=1),
+            nn.ReLU(inplace=True),
+            nn.LocalResponseNorm(5, alpha=0.0001, beta=0.75, k=2),
+            nn.MaxPool2d(3, stride=2),
+            nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(inplace=True),
+            nn.LocalResponseNorm(5, alpha=0.0001, beta=0.75, k=2),
+            nn.MaxPool2d(3, stride=2),
+            nn.Dropout2d(p=0.3),
+            nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, stride=2),
+            nn.Dropout2d(p=0.3),
+        )
+        # Defining the fully connected layers
+        self.fc1 = nn.Sequential(
+            nn.Linear(30976, 1024),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.5),
+            nn.Linear(1024, 128),
+            nn.ReLU(inplace=True),
+            nn.Linear(128, 2),
+        )
+
+    def forward_once(self, x):
+        # Forward pass
+        output = self.cnn1(x)
+        output = output.view(output.size()[0], -1)
+        output = self.fc1(output)
+        return output
+
+    def forward(self, input1, input2):
+        # forward pass of input 1
+        output1 = self.forward_once(input1)
+        # forward pass of input 2
+        output2 = self.forward_once(input2)
+        return output1, output2
+
+
+# 테스트
+
+# set the device to cuda
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = SiameseNetwork().cuda()
+# torch.save(model.state_dict(), "model.pt")
+# print("Model Saved Successfully")
+
+# Load trained model
+model.load_state_dict(torch.load("model.pt"))
+
+# Load the test dataset
+test_dataset = SiameseDataset(
+    training_dir=jeong_dir,
+    transform=transforms.Compose(
+        [transforms.Resize((105, 105)), transforms.ToTensor()]
+    ),
+)
+
+test_dataloader = DataLoader(test_dataset, num_workers=6, batch_size=1, shuffle=True)
+# test the network
+count = 0
+for i, data in enumerate(test_dataloader, 0):
+    x0, x1, label = data
+    concat = torch.cat((x0, x1), 0)
+    output1, output2 = model(x0.to(device), x1.to(device))
+
+    eucledian_distance = F.pairwise_distance(output1, output2)
+
+    if label == torch.FloatTensor([[0]]):
+        label = "Original Pair Of Signature"
+    else:
+        label = "Forged Pair Of Signature"
+
+    imshow(torchvision.utils.make_grid(concat))
+    print("Predicted Eucledian Distance:-", eucledian_distance.item())
+    print("Actual Label:-", label)
+    count = count + 1
+    if count == 10:
+        break
---
+++ siamese-net/testWithOrigin.py
@@ -0,0 +1,274 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def imshow(img, text=None, should_save=False):
+    npimg = img.numpy()
+    plt.axis("off")
+    if text:
+        plt.text(
+            75,
+            8,
+            text,
+            style="italic",
+            fontweight="bold",
+            bbox={"facecolor": "white", "alpha": 0.8, "pad": 10},
+        )
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+def show_plot(iteration, loss):
+    plt.plot(iteration, loss)
+    plt.show()
+
+
+import torch
+import torch.nn as nn
+import numpy as np
+import torch.nn.functional as F
+
+
+class ContrastiveLoss(nn.Module):
+    "Contrastive loss function"
+
+    def __init__(self, margin=2.0):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+
+    def forward(self, output1, output2, label):
+        euclidean_distance = F.pairwise_distance(output1, output2)
+        loss_contrastive = torch.mean(
+            (1 - label) * torch.pow(euclidean_distance, 2)
+            + (label)
+            * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2)
+        )
+
+        return loss_contrastive
+
+
+# import the necessary libraries
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as utils
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, Dataset
+import matplotlib.pyplot as plt
+import torchvision.utils
+import torchvision
+from torch.autograd import Variable
+from PIL import Image
+import PIL.ImageOps
+import os
+
+# load dataset
+training_dir = "/content/drive/MyDrive/siamese-net/content/sign_data/train"
+testing_dir = "/content/drive/MyDrive/siamese-net/content/sign_data/test"
+training_csv = "/content/drive/MyDrive/siamese-net/content/sign_data/train_data.csv"
+testing_csv = "/content/drive/MyDrive/siamese-net/content/sign_data/test_data.csv"
+batch_size = 32
+epochs = 20
+
+# transforms = transforms.Compose([transforms.Resize((100, 100)), transforms.ToTensor()])
+
+
+# preprocessing and loading the dataset
+class SiameseDataset:
+    def __init__(self, training_csv=None, training_dir=None, transform=None):
+        # used to prepare the labels and images path
+        self.train_df = pd.read_csv(training_csv)
+        self.train_df.columns = ["image1", "image2", "label"]
+        self.train_dir = training_dir
+        self.transform = transform
+
+    def __getitem__(self, index):
+        # getting the image path
+        image1_path = os.path.join(self.train_dir, self.train_df.iat[index, 0])
+        image2_path = os.path.join(self.train_dir, self.train_df.iat[index, 1])
+        # Loading the image
+        img0 = Image.open(image1_path)
+        img1 = Image.open(image2_path)
+        img0 = img0.convert("L")
+        img1 = img1.convert("L")
+        # Apply image transformations
+        if self.transform is not None:
+            img0 = self.transform(img0)
+            img1 = self.transform(img1)
+        return (
+            img0,
+            img1,
+            torch.from_numpy(
+                np.array([int(self.train_df.iat[index, 2])], dtype=np.float32)
+            ),
+        )
+
+    def __len__(self):
+        return len(self.train_df)
+
+
+# 이제 데이터세트를 사전 처리한 후 PyTorch에서 Dataloader 클래스를 사용하여 데이터세트를 로드
+# 변환 기능을 사용하여 계산 목적으로 이미지 크기를 높이와 너비의 105픽셀로 줄인다.
+
+# Load the the dataset from raw image folders
+siamese_dataset = SiameseDataset(
+    training_csv,
+    training_dir,
+    transform=transforms.Compose(
+        [transforms.Resize((105, 105)), transforms.ToTensor()]
+    ),
+)
+
+# 이제 Pytorch에서 신경망을 만들어 보겠다.
+# Signet 논문에 설명된 것과 유사한 신경망 아키텍처를 사용한다.
+
+
+# create a siamese network
+class SiameseNetwork(nn.Module):
+    def __init__(self):
+        super(SiameseNetwork, self).__init__()
+        # Setting up the Sequential of CNN Layers
+        self.cnn1 = nn.Sequential(
+            nn.Conv2d(1, 96, kernel_size=11, stride=1),
+            nn.ReLU(inplace=True),
+            nn.LocalResponseNorm(5, alpha=0.0001, beta=0.75, k=2),
+            nn.MaxPool2d(3, stride=2),
+            nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(inplace=True),
+            nn.LocalResponseNorm(5, alpha=0.0001, beta=0.75, k=2),
+            nn.MaxPool2d(3, stride=2),
+            nn.Dropout2d(p=0.3),
+            nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, stride=2),
+            nn.Dropout2d(p=0.3),
+        )
+        # Defining the fully connected layers
+        self.fc1 = nn.Sequential(
+            nn.Linear(30976, 1024),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.5),
+            nn.Linear(1024, 128),
+            nn.ReLU(inplace=True),
+            nn.Linear(128, 2),
+        )
+
+    def forward_once(self, x):
+        # Forward pass
+        output = self.cnn1(x)
+        output = output.view(output.size()[0], -1)
+        output = self.fc1(output)
+        return output
+
+    def forward(self, input1, input2):
+        # forward pass of input 1
+        output1 = self.forward_once(input1)
+        # forward pass of input 2
+        output2 = self.forward_once(input2)
+        return output1, output2
+
+
+# 손실 함수
+
+
+class ContrastiveLoss(torch.nn.Module):
+    """
+    Contrastive loss function.
+    Based on:
+    """
+
+    def __init__(self, margin=1.0):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+
+    def forward(self, x0, x1, y):
+        # euclidian distance
+        diff = x0 - x1
+        dist_sq = torch.sum(torch.pow(diff, 2), 1)
+        dist = torch.sqrt(dist_sq)
+
+        mdist = self.margin - dist
+        dist = torch.clamp(mdist, min=0.0)
+        loss = y * dist_sq + (1 - y) * torch.pow(dist, 2)
+        loss = torch.sum(loss) / 2.0 / x0.size()[0]
+        return loss
+
+
+# Load the dataset as pytorch tensors using dataloader
+train_dataloader = DataLoader(
+    siamese_dataset, shuffle=True, num_workers=8, batch_size=batch_size
+)
+
+# Declare Siamese Network
+net = SiameseNetwork().cuda()
+# Decalre Loss Function
+criterion = ContrastiveLoss()
+# Declare Optimizer
+optimizer = torch.optim.Adam(net.parameters(), lr=1e-3, weight_decay=0.0005)
+
+
+# train the model
+def train():
+    loss = []
+    counter = []
+    iteration_number = 0
+    for epoch in range(1, epochs):
+        for i, data in enumerate(train_dataloader, 0):
+            img0, img1, label = data
+            img0, img1, label = img0.cuda(), img1.cuda(), label.cuda()
+            optimizer.zero_grad()
+            output1, output2 = net(img0, img1)
+            loss_contrastive = criterion(output1, output2, label)
+            loss_contrastive.backward()
+            optimizer.step()
+        print("Epoch {}\n Current loss {}\n".format(epoch, loss_contrastive.item()))
+        iteration_number += 10
+        counter.append(iteration_number)
+        loss.append(loss_contrastive.item())
+    show_plot(counter, loss)
+    return net
+
+
+# set the device to cuda
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = train()
+torch.save(model.state_dict(), "model.pt")
+print("Model Saved Successfully")
+
+
+# 테스트
+
+# Load the test dataset
+test_dataset = SiameseDataset(
+    training_csv=testing_csv,
+    training_dir=testing_dir,
+    transform=transforms.Compose(
+        [transforms.Resize((105, 105)), transforms.ToTensor()]
+    ),
+)
+
+test_dataloader = DataLoader(test_dataset, num_workers=6, batch_size=1, shuffle=True)
+# test the network
+count = 0
+for i, data in enumerate(test_dataloader, 0):
+    x0, x1, label = data
+    concat = torch.cat((x0, x1), 0)
+    output1, output2 = model(x0.to(device), x1.to(device))
+
+    eucledian_distance = F.pairwise_distance(output1, output2)
+
+    if label == torch.FloatTensor([[0]]):
+        label = "Original Pair Of Signature"
+    else:
+        label = "Forged Pair Of Signature"
+
+    imshow(torchvision.utils.make_grid(concat))
+    print("Predicted Eucledian Distance:-", eucledian_distance.item())
+    print("Actual Label:-", label)
+    count = count + 1
+    if count == 10:
+        break
